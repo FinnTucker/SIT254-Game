f3c06136c142020ae3aefa8724e248eae4fc3b67
FinnTucker <s216009328@deakin.edu.au>
2024-09-22 18:34:26 +1000

    mid-way through design of prototype level to showcase included mechanics

---
+++ scripts/title_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+@onready var start_game: Button = $StartGame
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$StartGame.connect("pressed", Callable(self, "_on_start_button_pressed"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
